8652a1b96cf59ade611d1e69a7e423b4125fa0e1
Gil Tayar <gilt@wix.com>
2016-08-31 18:33:03 +0300

    index4 - done

---
+++ index4.js
@@ -0,0 +1,24 @@
+const {describe, it} = require('mocha')
+const jsdom = require('jsdom')
+const assert = require('assert')
+
+describe('react app', function() {
+  it('should increment the counter', function() {
+    global.window = jsdom.jsdom('<div id="mount-point"><div>').defaultView
+    global.document = window.document
+    global.navigator = window.navigator
+
+    const reactApp = require('./ReactApp.js')
+    const ReactTestUtils = require('react-addons-test-utils');
+
+    const $ = require('jquery-slim')
+
+    reactApp($('#mount-point')[0])
+
+    ReactTestUtils.Simulate.click($('#mount-point .inc')[0])
+
+    const text = $('#mount-point .counter-value').text()
+
+    assert.equal(text, '1')
+  })
+})
