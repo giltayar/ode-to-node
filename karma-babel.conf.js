416c9668c1992159db8084f020552821900306c5
Gil Tayar <gilt@wix.com>
2016-09-13 12:19:04 +0300

    karma running screenshots added

--- karma-babel.conf.js
+++ karma-babel.conf.js
@@ -27,6 +27,9 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      // For babel transpiling. Not enough...
+      'src/**/*.js': ['babel'],
+      'test/**/*.js': ['babel']
     },
 
     // test results reporter to use
